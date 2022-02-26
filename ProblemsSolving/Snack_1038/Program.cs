0d4d6de8e4fdb2ee06b2c7d11801ead293571ba2
MithunCh <mithun.howlader222@gmail.com>
2022-02-26 08:26:03 +0600

    Snack_1038 done

---
+++ ProblemsSolving/Snack_1038/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Snack_1038
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+            var split = input.Split(' ');
+            var X = int.Parse(split[0]);
+            var Y = int.Parse(split[1]);
+
+            if(X == 1)
+            {
+                Console.WriteLine($"Total: R$ {string.Format("{0:0.00}", 4 * Y)}");
+            }
+            else if(X == 2)
+            {
+
+                Console.WriteLine($"Total: R$ {string.Format("{0:0.00}", 4.50 * Y)}");
+            }
+            else if (X == 3)
+            {
+                Console.WriteLine($"Total: R$ {string.Format("{0:0.00}", 5 * Y)}");
+            }
+            else if (X == 4)
+            {
+                Console.WriteLine($"Total: R$ {string.Format("{0:0.00}", 2 * Y)}");
+            }
+            else if (X == 5)
+            {
+                Console.WriteLine($"Total: R$ {string.Format("{0:0.00}", 1.50 * Y)}");
+            }
+        }
+    }
+}
